fe2fcb38e6271c2bf97ee965c00f65a9c70a997a
JagdpantherK <jagpanthertitnium@gmail.com>
2022-12-25 02:03:33 +0600

    Initial commit

---
+++ parser/parser.py
@@ -0,0 +1,172 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from data import username, password
+import time
+import random
+from selenium.common.exceptions import NoSuchElementException
+import requests
+import os
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+
+class InstagramBot():
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        options = Options()
+        options.add_argument("start-maximized")
+        options.add_argument("disable-info bars")
+        self.browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    def close_browser(self):
+
+        self.browser.close()
+        self.browser.quit()
+
+    def login(self):
+
+        browser = self.browser
+        browser.get('https://www.instagram.com')
+        time.sleep(random.randrange(3, 5))
+
+        username_input = browser.find_element(By.NAME, 'username')
+        username_input.clear()
+        username_input.send_keys(username)
+
+        time.sleep(2)
+
+        password_input = browser.find_element(By.NAME, 'password')
+        password_input.clear()
+        password_input.send_keys(password)
+
+        password_input.send_keys(Keys.ENTER)
+        time.sleep(10)
+
+    # метод проверяет по xpath существует ли элемент на странице
+    def xpath_exists(self, url):
+
+        browser = self.browser
+        try:
+            browser.find_element(By.XPATH, url)
+            exist = True
+        except NoSuchElementException:
+            exist = False
+        return exist
+
+    # метод ставит лайк на пост по прямой ссылке
+
+    def get_all_posts_urls(self, userpage):
+
+        browser = self.browser
+        browser.get(userpage)
+        time.sleep(4)
+
+        wrong_userpage = "/html/body/div[1]/section/main/div/h2"
+        if self.xpath_exists(wrong_userpage):
+            print("Такого пользователя не существует, проверьте URL")
+            self.close_browser()
+        else:
+            print("Пользователь успешно найден, ставим лайки!")
+            time.sleep(2)
+
+            posts_count = browser.find_element(By.XPATH,
+                                                   "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[1]/div/span/span").text
+
+            int_posts_count = int(''.join([n for n in posts_count if n.isdigit()]))
+            loops_count = int(int_posts_count / 86)
+            print(loops_count)
+
+            posts_urls = []
+            for i in range(0, loops_count):
+                hrefs = browser.find_elements(By.TAG_NAME, 'a')
+                hrefs = [item.get_attribute('href') for item in hrefs if "/p/" in item.get_attribute('href')]
+
+                for href in hrefs:
+                    posts_urls.append(href)
+
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(random.randrange(2, 4))
+                print(f"Итерация #{i}")
+
+            file_name = userpage.split("/")[-2]
+
+            with open(f'{file_name}.txt', 'a') as file:
+                for post_url in posts_urls:
+                    file.write(post_url + "\n")
+
+            set_posts_urls = set(posts_urls)
+            set_posts_urls = list(set_posts_urls)
+
+            with open(f'{file_name}_set.txt', 'a') as file:
+                for post_url in set_posts_urls:
+                    file.write(post_url + '\n')
+
+    def download_userpage_content(self, userpage):
+
+        browser = self.browser
+        self.get_all_posts_urls(userpage)
+        file_name = userpage.split("/")[-2]
+        time.sleep(4)
+        browser.get(userpage)
+        time.sleep(4)
+
+        # создаём папку с именем пользователя для чистоты проекта
+        if os.path.exists(f"{file_name}"):
+            print("Папка уже существует!")
+        else:
+            os.mkdir(file_name)
+
+        img_and_video_src_urls = []
+        with open(f'{file_name}_set.txt') as file:
+            urls_list = file.readlines()
+
+            for post_url in urls_list[0:2500]:
+                try:
+                    browser.get(post_url)
+                    time.sleep(4)
+
+                    img_src = "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/div[1]/article/div/div[1]/div/div[1]/div[2]/div/div/div/ul/li[2]/div/div/div/div/div[1]/img"
+                    video_src = "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div[1]/div[1]/article/div/div[1]/div/div/div/div/div/div/div/video"
+                    post_id = post_url.split("/")[-2]
+
+                    if self.xpath_exists(img_src):
+                        img_src_url = browser.find_element(By.XPATH, img_src).get_attribute("src")
+                        img_and_video_src_urls.append(img_src_url)
+
+                        # сохраняем изображение
+                        get_img = requests.get(img_src_url)
+                        with open(f"{file_name}/{file_name}_{post_id}_img.jpg", "wb") as img_file:
+                            img_file.write(get_img.content)
+
+                    elif self.xpath_exists(video_src):
+                        video_src_url = browser.find_element(By.XPATH, video_src).get_attribute("src")
+                        img_and_video_src_urls.append(video_src_url)
+
+                        # сохраняем видео
+                        get_video = requests.get(video_src_url, stream=True)
+                        with open(f"{file_name}/{file_name}_{post_id}_video.mp4", "wb") as video_file:
+                            for chunk in get_video.iter_content(chunk_size=1024 * 1024):
+                                if chunk:
+                                    video_file.write(chunk)
+                    else:
+                        # print("Упс! Что-то пошло не так!")
+                        img_and_video_src_urls.append(f"{post_url}, нет ссылки!")
+                    print(f"Контент из поста {post_url} успешно скачан!")
+
+                except Exception as ex:
+                    print(ex)
+                    self.close_browser()
+
+            self.close_browser()
+
+        with open(f'{file_name}/{file_name}_img_and_video_src_urls.txt', 'a') as file:
+            for i in img_and_video_src_urls:
+                file.write(i + "\n")
+
+
+my_bot = InstagramBot(username, password)
+my_bot.login()
+my_bot.download_userpage_content("https://www.instagram.com/userpage/")
